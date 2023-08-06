576d122e36b4c068354306e98a598320f0b229a4
Sebastian <serranosebastian23@gmail.com>
2023-08-05 21:06:58 -0700

    enumerate() method

---
+++ enumerate.py
@@ -0,0 +1,7 @@
+names = ['sebastian', 'zoey', 'jacob', 'frank', 'donny']
+
+for index, name in enumerate(names, start=1):
+    print(index, name)
+
+# enumerate() helps you keep track of the index as well as the value
+# output: 1 sebastian, 2 zoey, 3 jacob, 4 frank, 5 donny
